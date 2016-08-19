0951197b5a6e80297552b36b1d38ff80b1599838
Paul Grever <paulgrever@gmail.com>
2016-08-19 11:46:51 -0600

    updated registry to work with custom go theme

--- register.js
+++ register.js
@@ -1,4 +1,6 @@
-ReactionCore.registerPackage({
+import { Reaction } from '/server/api';
+
+Reaction.registerPackage({
   label: 'Shipstation',
   name: 'reaction-shipstation',
   icon: 'fa fa-cog',
@@ -12,7 +14,7 @@ ReactionCore.registerPackage({
     container: 'getoutfitted',
     icon: 'fa fa-cog',
     template: 'shipStationDashboard',
-    workflow: 'coreWorkflow',
+    workflow: 'shipStationWorkflow',
     priority: 3
   }, {
     route: '/dashboard/shipstation/settings',
@@ -20,5 +22,32 @@ ReactionCore.registerPackage({
     label: 'Shipstation Settings',
     name: 'shipstationSettings',
     template: 'shipstationSettings'
+  }],
+  layout: [{
+    workflow: "shipStationWorkflow",
+    layout: "coreLayout",
+    theme: "default",
+    enabled: true,
+    structure: {
+      template: "shipStationDashboard",
+      layoutHeader: "goLayoutHeader",
+      layoutFooter: "goLayoutFooter",
+      notFound: "goNotFound",
+      dashboardControls: "dashboardControls",
+      adminControlsFooter: "adminControlsFooter"
+    }
+  }, {
+    workflow: "shipStationWorkflow",
+    layout: "getoutfittedLayout",
+    theme: "default",
+    enabled: true,
+    structure: {
+      template: "shipStationDashboard",
+      layoutHeader: "goLayoutHeader",
+      layoutFooter: "goLayoutFooter",
+      notFound: "goNotFound",
+      dashboardControls: "dashboardControls",
+      adminControlsFooter: "adminControlsFooter"
+    }
   }]
 });
